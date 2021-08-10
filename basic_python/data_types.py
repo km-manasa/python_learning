b906e6a8b5cd14492aaff47278b8465d7619341f
km-manasa <manasakm88@gmail.com>
2021-08-10 21:18:51 +0530

    data types

---
+++ basic_python/data_types.py
@@ -0,0 +1,36 @@
+a = 8
+print(type(a))
+
+b = 4.5
+print(type(b))
+
+c = 4 + 5j
+print(type(c))
+
+r = a > b
+print(type(r))
+
+ls = [12, 45, 78, 29]
+print(type(ls))
+
+t = (67, 32, 87, 63, 11)
+print(type(t))
+
+s = {23, 87, 46, 61, 19}
+print(type(s))
+
+str = 'Manasa'
+print(type(str))
+
+range(0, 10)
+print(range)
+print(list(range(10)))
+print(list(range(2, 11, 2)))
+
+d = {'m': 54, 'n': 76, 'o': 67}
+print(d)
+print(d.keys())
+print(d.values())
+print(d['m'])
+print(d.get('n'))
+
