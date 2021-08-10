f6adb5c92706767453dfafbb96308d6662bf94e2
km-manasa <manasakm88@gmail.com>
2021-08-10 20:38:17 +0530

    variables

---
+++ basic_python/variables.py
@@ -0,0 +1,15 @@
+cont = 3
+print(id(cont))
+name = 'Manasa'
+print(id(name))
+a = 15
+b = a
+print(a)
+print(b)
+print(id(a))
+print(id(b))
+print(id(15))
+type(a)
+print(type(a))
+f=2.87
+print(type(f))
