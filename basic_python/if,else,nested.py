d292f2ebfe8ef92d367dae835385a48233ec986b
km-manasa <manasakm88@gmail.com>
2021-08-11 21:08:14 +0530

    if and else nested ifs

---
+++ basic_python/if,else,nested.py
@@ -0,0 +1,13 @@
+x = int(input("Enter a value"))
+r = x % 2
+if r==0:
+    print('Even')
+    if x<5:
+        print('Greater number')
+    else:
+        print('Not greater')
+else:
+    print('Odd')
+
+
+
