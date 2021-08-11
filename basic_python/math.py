2ecf08b4fb7c42b52cef084936e571eb76a597bc
km-manasa <manasakm88@gmail.com>
2021-08-11 20:28:54 +0530

    importing

---
+++ basic_python/math.py
@@ -0,0 +1,18 @@
+import math
+
+x = math.sqrt(25)
+print(x)
+
+x = (math.floor(2.9))
+print(x)
+
+x = (math.ceil(2.4))
+print(x)
+
+x = (math.pow(3, 3))
+print(x)
+
+import math as m
+x = m.sqrt(25)
+print(x)
+
